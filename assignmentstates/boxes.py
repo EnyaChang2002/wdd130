683191b180350d5bafe196e1d9d106d66808bc5f
EnyaChang2002 <yyy2011taiwan@gmail.com>
2023-09-22 13:31:07 +0800

    side plan added

--- assignmentstates/boxes.py
+++
@@ -1,7 +0,0 @@
-import math
-
-num = int(input('Enter the number of items: '))
-per_box = int(input('Enter the number of items per box: '))
-boxes = math.ceil(num / per_box)
-
-print(f'For {num} items,  packing {per_box} items in each box, you will need {boxes} boxes.')
